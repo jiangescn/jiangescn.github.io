7e564b9719c903b5db18a3a3e3625ad228e541d1
jianges <2660998832@qq.com>
2026-01-13 19:33:35 +0800

    del

--- blog.config.ts
+++ blog.config.ts
@@ -1,26 +1,26 @@
 import type { FeedEntry } from './app/types/feed'
 
 const basicConfig = {
-	title: '纸鹿摸鱼处',
-	subtitle: '纸鹿至麓不知路，支炉制露不止漉',
+	title: '杂话铺子',
+	subtitle: '留此一方素纸，待岁生苔|',
 	// 长 description 利好于 SEO
-	description: '纸鹿本鹿的个人博客，分享技术与生活。“折腾不止，摸鱼生活——摸门🙏🏻”。纸鹿是一名开源爱好者，结识了许多志同道合的朋友。这个博客记录了他在生活和技术学习中的点滴经历，充满启发与思考。网站界面简洁美观，内容丰富实用，人气互动活跃，涵盖了编程、生活、学习等多个领域，为读者提供了卓越的阅读体验。',
+	description: '兹将心事，化入岁暮之砚。不求标榜于竹帛，惟愿苔痕过雪，鸿影印沙，为逝川留一二实证。倘他年风翻此页，便算在人间，多活了一遭。',
 	author: {
-		name: '纸鹿本鹿',
-		avatar: 'https://www.zhilu.site/api/avatar.png',
-		email: 'hi@zhilu.cyou',
-		homepage: 'https://www.zhilu.site/',
+		name: '姜一',
+		avatar: 'https://www.zhilu.https://jianges.com/wp-content/uploads/2025/12/1765434218-jianges.jpg/api/avatar.png',
+		email: '2660998832@qq.com',
+		homepage: 'https://jianges.com/',
 	},
 	copyright: {
 		abbr: 'CC BY-NC-SA 4.0',
 		name: '署名-非商业性使用-相同方式共享 4.0 国际',
 		url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh-hans',
 	},
-	favicon: 'https://www.zhilu.site/api/icon.png',
+	favicon: 'https://jianges.com/wp-content/uploads/2025/12/1765434218-jianges.jpg',
 	language: 'zh-CN',
 	timeEstablished: '2019-07-19',
 	timezone: 'Asia/Shanghai',
-	url: 'https://blog.zhilu.site/',
+	url: 'https://jianges.com/',
 	defaultCategory: '未分类',
 }
 
@@ -86,7 +86,7 @@ const blogConfig = {
 /** 用于生成 OPML 和友链页面配置 */
 export const myFeed: FeedEntry = {
 	author: blogConfig.author.name,
-	sitenick: '摸鱼处',
+	sitenick: '杂话铺',
 	title: blogConfig.title,
 	desc: blogConfig.subtitle || blogConfig.description,
 	link: blogConfig.url,
